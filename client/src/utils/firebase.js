8cd04fdc9f14bec973ae6ba43a08730468487fa6
Pragatii2606 <pragatiprince2606@gmail.com>
2026-06-19 03:41:48 +0530

    firebase updated

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -1,13 +1,21 @@
 
 import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+//   authDomain: "fir-dc891.firebaseapp.com",
+//   projectId: "fir-dc891",
+//   storageBucket: "fir-dc891.firebasestorage.app",
+//   messagingSenderId: "253714282822",
+//   appId: "1:253714282822:web:7899bdc02814ff52ff8d47"
+// };
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "fir-dc891.firebaseapp.com",
-  projectId: "fir-dc891",
-  storageBucket: "fir-dc891.firebasestorage.app",
-  messagingSenderId: "253714282822",
-  appId: "1:253714282822:web:7899bdc02814ff52ff8d47"
+  authDomain: "interviewiqai-d6f22.firebaseapp.com",
+  projectId: "interviewiqai-d6f22",
+  storageBucket: "interviewiqai-d6f22.firebasestorage.app",
+  messagingSenderId: "1025758762174",
+  appId: "1:1025758762174:web:b242214d0607fe1216faef"
 };
 
 const app = initializeApp(firebaseConfig);
